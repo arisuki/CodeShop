a2d41af136b1a4a3aaf4ae9ab210c410bd78e437
arisuki <alice.stancu@gmail.com>
2023-06-26 15:59:37 -0400

    begin item listing mockup
    Co-authored-by: Alina Shumakova <aashumakova@gmail.com@gmail.com>
    Co-authored-by: Ian Abbene <ianabbene@gmail.com>

--- src/components/ShopListItem/ShopListItem.jsx
+++ src/components/ShopListItem/ShopListItem.jsx
@@ -1,8 +1,16 @@
+import {Link} from "react-router-dom"
+
 export default function ShopListItem({item}) {
     return (
         <>
-        <div>Shop item with pics</div>
+        {/* <Link to = {{pathname: {item.link.toString}} target = "_blank"} ></Link> */}
+        <a href="">
+        {/* <img src="{item.image}" alt="" /> */}
         <div>{item.name}</div>
+        </a>
+        <div>${item.price}</div>
+        <div>{item.description}</div>
+       
         </>
     );
 }
