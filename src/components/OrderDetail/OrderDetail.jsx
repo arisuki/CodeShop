164723e69ec5f7eb652b5598f8aaacf86b9ed387
aashumakova <aashumakova@gmail.com>
2023-06-26 12:13:24 -0700

    front-end stubbed up, Co-authored-by: Alice Stancu <alice.stancu@gmail.com> Co-authored-by: Ian Abbene <ianabbene@gmail.com>

---
+++ src/components/OrderDetail/OrderDetail.jsx
@@ -0,0 +1,7 @@
+export default function OrderDetail() {
+    return (
+<>
+line items will go here aka order detail page
+</>
+    );
+}
---
+++ src/components/ShopList/ShopList.jsx
@@ -0,0 +1,14 @@
+import ShopListItem from "../ShopListItem/ShopListItem";
+
+export default function ShopList({ items }) {
+    const ShopListJsx = items.map((item, idx) => 
+    <ShopListItem 
+        item={item} key={idx}/>
+    )
+    return (
+        <>
+        <div>will map through each menu list item</div>
+        {ShopListJsx}
+        </>
+    )
+}
