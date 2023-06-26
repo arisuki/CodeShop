e90935099586760ba7e3d7aa31f949bd44df0a60
iabbene <ianabbene@gmail.com>
2023-06-26 10:15:22 -0500

    Models complete
    Co-authored-by: Alina Shumakova <aashumakova@gmail.com@gmail.com>
    Co-authored-by: Alice Stancu <alice.stancu@gmail.com>

--- models/itemSchema.js
+++ models/itemSchema.js
@@ -2,7 +2,18 @@ const Schema = require('mongoose').Schema;
 
 const itemSchema = new Schema({
     name: { type: String, required: true },
-    emoji: String,
+    image: {
+        type: String,
+        default: ""
+    },
+    description: {
+        type: String,
+        default: ""
+    },
+    link: {
+        type: String,
+        default: ""
+    },
     category: { type: Schema.Types.ObjectId, ref: 'Category' },
     price: { type: Number, required: true }
 }, {
