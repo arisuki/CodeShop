71a98009e2994c0f2afc5944667274b5cec1fa66
arisuki <alice.stancu@gmail.com>
2023-06-28 13:52:28 -0400

    hitting shoplistitemshow

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -5,23 +5,19 @@ import { useState, useEffect } from 'react';
 import AuthPage from "../AuthPage/AuthPage";
 import NavBar from "../../components/NavBar/NavBar";
 import Shop from "../Shop/Shop";
-import ShopListItemShow from "../../components/ShopList/ShopList";
+import ShopListItemShow from "../../components/ShopListItemShow/ShopListItemShow";
 // import { getUser } from "../../utilities/users-service"
 // import NewOrderPage from '../NewOrderPage/NewOrderPage';
 // import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-
-
 
 export default function App() {
   const [user, setUser] = useState("Alice"); //getUser()
   console.log("this is user in App", user);
   const [shopItems, setShopItems] = useState([])
 
-
   useEffect(() =>{
     getItems()
 }, [])
-
     async function getItems() {
         const items = await itemsAPI.getAll();
         // categoriesRef.current = [...new Set(items.map(item => item.category.name))];
@@ -35,8 +31,7 @@ export default function App() {
         <>
           <NavBar user={user} setUser={setUser} />
           <Routes>
-            <Route path="/" element={<Shop shopItems={shopItems} />} />
-            <Route path="/shop" element={<Shop shopItems={shopItems}/>} />
+            <Route exact path="/" element={<Shop shopItems={shopItems} />} />
             <Route
               path="/shop/items/:id"
               element={<ShopListItemShow shopItems={shopItems} />}
