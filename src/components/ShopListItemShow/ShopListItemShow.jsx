87868ab787b8e0006364c2038a9f9a7e82061444
arisuki <alice.stancu@gmail.com>
2023-06-28 10:42:40 -0400

    deploy party

--- src/components/ShopListItemShow/ShopListItemShow.jsx
+++ src/components/ShopListItemShow/ShopListItemShow.jsx
@@ -1,8 +1,16 @@
-export default function ShopListItemShow({shopItem}){
+import {useParams} from "react-router-dom"
+
+export default function ShopListItemShow({shopItems}){
+const {itemId} = useParams()
+console.log("itemId in url", {itemId})
+
+const item = shopItems.find((item) => item._id === itemId);
+console.log("item const", item);
+
 return(
     <div>
         Item details page:
-        {shopItem.name}
+        
     </div>
 )
 }
