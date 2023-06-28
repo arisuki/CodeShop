51aa3f85a31680c22f639992f70415183252fb87
arisuki <alice.stancu@gmail.com>
2023-06-28 10:59:28 -0400

    update deployed server url

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,6 +1,6 @@
 import sendRequest from './send-request';
 // we need a base path that we can use to refer our requests to the locatin of our routes
-const BASE_URL = "./api/users";
+const BASE_URL = "https://codeshop-api.onrender.com/api/users";
 
 export function signUp(userData) {
   return sendRequest(BASE_URL, 'POST', userData);
@@ -10,6 +10,6 @@ export function login(credentials) {
   return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
 }
 
-export function checkToken(){
+export function checkToken() {
   return sendRequest(`${BASE_URL}/check-token`);
 }
